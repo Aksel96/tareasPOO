c2bec28a74074bd76a61a1446e7752e030bbf2c3
aksel96 <jonhaksel@gmail.com>
2023-02-24 10:54:20 -0600

    Correccion de comentarios

--- Tarea2/src/main/java/ico/fes/Tarea2.java
+++ Tarea2/src/main/java/ico/fes/Tarea2.java
@@ -1,3 +1,7 @@
+
+package ico.fes;
+
+import java.util.Scanner;
 /*
 #######################################################################################################################
 ##                                    UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO                                        ##
@@ -8,10 +12,6 @@
 ##                                           AKSEL VILLELA ANDRADE                                                   ##
 #######################################################################################################################
 */
-package ico.fes;
-
-import java.util.Scanner;
-
 public class Tarea2 {
     public static void main(String[] args) {
         //Inicio del programa, pedimos la cadena, la asignamos a una variable y limpiamos la basura con trim()
@@ -21,8 +21,8 @@ public class Tarea2 {
 
         //Creamos un arreglo de enteros para guardar cada elemento de la cadena que ingrese el usuario
         int[] listaDecimales = new int[entrada.length()];
-        boolean verificadorOrden = false; //Variable para verificar si estan ordenados
-        int contador = 0; //Contador
+        boolean verificadorOrden = true; //Variable para verificar si estan ordenados
+
 
         //Primer bucle para convertir cada elemento de la cadena a entero y almacenarlo en un arreglo de enteros
         for (int i = 0; i < entrada.length(); i++) {
@@ -31,16 +31,13 @@ public class Tarea2 {
         }
 
         //Segundo bucle para verificar que el segundo elemento sea el sucesor directo del primero
-        for (int i = 1; i < listaDecimales.length - 1; i++) {
-            int elemento = listaDecimales[i - 1]; //asignamos el elemento a una variable
-            //Verificamos que si el elemento es igual al siguiente elemento menos uno , sumamos en uno al contador
-            if (elemento == listaDecimales[i] - 1){
-                 contador += 1;
+        for (int i = 0; i < listaDecimales.length - 1; i++) {
+            //Restamos el indice actual menos el siguiente y si el resultado no es 1, no es el sucesor consecutivo
+            if (listaDecimales[i +1] - listaDecimales[i] != 1){
+                verificadorOrden= false;
+                break;
             }
-            //Si el contador es igual a la extension del arreglo menos dos, decimos que el arreglo esta ordenado
-            if (contador == listaDecimales.length - 2){ // Es -2 ya que el arreglo empieza desde cero
-                verificadorOrden = true;
-            }
+
         }
         //Imprimimos el resultado
         System.out.println("Resultado de la comprobacion: " + verificadorOrden);
